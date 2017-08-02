820fb0f5a29a046e32718ce2259d74325f92453a
Trumpower, Josiah <josiah.trumpower@cengage.com>
2017-08-02 10:34:16 -0400

    Added new images for couples area

--- src/app/js/components/couple.component.ts
+++ src/app/js/components/couple.component.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-couple-component',
   templateUrl: '../../html/couple.component.html',
+  styleUrls: ['../../css/couple.component.css']
 })
 export class CoupleComponent {
 
